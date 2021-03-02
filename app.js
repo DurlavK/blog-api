80b414adfe9dae24642f6fc0340c5d440cbfdcf7
durlavk <durlavk98@gmail.com>
2021-03-02 19:20:26 +0530

    organized app.js

--- app.js
+++ app.js
@@ -1,62 +1,27 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const path = require('path');
 const session = require('express-session');
-const passport = require('passport');
-const LocalStrategy = require('passport-local').Strategy;
 const cors = require('cors');
-const bcrypt = require('bcryptjs');
 var compression = require('compression');
 var helmet = require('helmet');
+const passport = require('passport');
+
+require('./config/passport');
 
 require('dotenv').config();
 
-var User = require('./models/User');
 
 var indexRouter = require('./routes/index');
 var apiRouter = require('./routes/api');
 
 var app = express();
 
-app.use(compression());
-app.use(helmet());
-
 mongoose.connect(process.env.MONGODB_URI, {useNewUrlParser: true, useUnifiedTopology: true});
 var db = mongoose.connection;
 db.on('err', console.error.bind(console, 'mongodb connection error'));
 
-passport.use(
-  new LocalStrategy((username, password, done) => {
-    User.findOne({ username: username }, (err, user) => {
-      if (err) { 
-        return done(err);
-      };
-      if (!user) {
-        return done(null, false, { message: "Incorrect Username" });
-      }
-      bcrypt.compare(password, user.password, (err, res) => {
-        if (res) {
-          // Successful login
-          return done(null, user);
-        } else {
-          // Passwords do not match
-          return done(null, false, {msg: 'Incorrect Password'});
-        }
-      });
-    });
-  })
-);
-
-passport.serializeUser(function(user, done) {
-  done(null, user.id);
-});
-
-passport.deserializeUser(function(id, done) {
-  User.findById(id, function(err, user) {
-    done(err, user);
-  });
-});
-
+app.use(compression());
+app.use(helmet());
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended:false}));
@@ -66,5 +31,12 @@ app.use(passport.session());
 
 app.use('/',indexRouter);
 app.use('/api',apiRouter);
+
+// Handle any unhandled error
+app.use((err, req, res, next) => {
+  if(err) {
+    res.status(500).json({message: 'Somethinf went wrong.'});
+  }
+});
 
 app.listen(process.env.PORT, ()=> console.log(`Server up on ${process.env.PORT}`));
